4d67542057522951f8b23491a28e288fe558a784
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-04-06 12:22:20 -0300

    add commands process

--- process/process.go
+++ process/process.go
@@ -6,6 +6,7 @@ import (
 	"git.resultys.com.br/framework/lower/net/loopback"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 type Proc struct {
@@ -13,12 +14,42 @@ type Proc struct {
 	state   *os.ProcessState
 }
 
+func Command(command string) {
+	if runtime.GOOS == "windows" {
+		Cmd(command)
+	} else {
+		Bash(command)
+	}
+}
+
+func Bash(command string) (*Proc, error) {
+	cmd := exec.Command("sh", "-c", command)
+	err := cmd.Run()
+	if err != nil {
+		log.Logger.Save(err.Error(), log.WARNING, loopback.IP())
+		return nil, errors.New("processo nao encontrado")
+	}
+
+	return &Proc{process: cmd.Process, state: cmd.ProcessState}, nil
+}
+
+func Cmd(command string) (*Proc, error) {
+	cmd := exec.Command("cmd", "/c", command)
+	err := cmd.Run()
+	if err != nil {
+		log.Logger.Save(err.Error(), log.WARNING, loopback.IP())
+		return nil, errors.New("processo nao encontrado")
+	}
+
+	return &Proc{process: cmd.Process, state: cmd.ProcessState}, nil
+}
+
 func Start(program string) (proc *Proc, err error) {
 	cmd := exec.Command(program)
 	err1 := cmd.Start()
 	if err1 != nil {
 		log.Logger.Save(err1.Error(), log.WARNING, loopback.IP())
-		return nil, erros.New("processo nao encontrado")
+		return nil, errors.New("processo nao encontrado")
 	}
 
 	return &Proc{process: cmd.Process, state: cmd.ProcessState}, nil
