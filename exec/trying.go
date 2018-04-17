08f953216a06b4fdbce95d2920de1f2b9d3b3906
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-04-17 17:00:09 -0300

    sync

--- exec/trying.go
+++ exec/trying.go
@@ -1,23 +1,33 @@
 package exec
 
 import (
+	"time"
+
 	"git.resultys.com.br/lib/lower/log"
 	"git.resultys.com.br/lib/lower/net/loopback"
 )
 
 // Trying tenta executar a função em até 'tentativas' vezes.
-// Caso ocorra erro chama o callback err
-// Ao final da execução com erro ou não invoka o callback finish
-func Trying(tentativas int, code func(), err func(string), finish func()) {
-
+// Caso ocorra successo chama o callback success
+// Caso ocorra error chama o callback error
+// Ao final do processo chama o callback finish
+func Trying(tentativas int, code func(), success func(), err func(), finish func()) {
+	b := false
 	for i := 0; i < tentativas; i++ {
 		Try(func() {
 			code()
 			i = 10000
+			b = true
 		}).Catch(func(message string) {
 			log.Logger.Save(message, log.WARNING, loopback.IP())
-			err(message)
+			time.Sleep(5 * time.Second)
 		})
+	}
+
+	if b {
+		success()
+	} else {
+		err()
 	}
 
 	finish()
