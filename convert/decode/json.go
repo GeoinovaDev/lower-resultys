97b5852d6e091b4b60c4f55d5bd871c1c10a42e6
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-05-08 15:01:42 -0300

    sync

--- convert/decode/json.go
+++ convert/decode/json.go
@@ -3,10 +3,16 @@ package decode
 import (
 	"bytes"
 	"encoding/json"
+
+	"git.resultys.com.br/lib/lower/log"
+	"git.resultys.com.br/lib/lower/net/loopback"
 )
 
 // JSON decode into object
 func JSON(str string, obj interface{}) {
 	b := bytes.NewBufferString(str)
-	json.NewDecoder(b).Decode(&obj)
+	err := json.NewDecoder(b).Decode(&obj)
+	if err != nil {
+		log.Logger.Save(err.Error(), log.WARNING, loopback.IP())
+	}
 }
