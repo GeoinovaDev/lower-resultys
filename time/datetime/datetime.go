c27b4f8b47818afb3ec7e5ba8a952b0d5569ab90
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-05-16 13:57:05 -0300

    sync

--- time/datetime/datetime.go
+++ time/datetime/datetime.go
@@ -1,6 +1,7 @@
 package datetime
 
 import (
+	"strconv"
 	"strings"
 	"time"
 
@@ -25,28 +26,18 @@ func Parse(datetime string) *Datetime {
 	return &Datetime{time: t}
 }
 
-// ParseUTC ...
-func ParseUTC(utc string) *Datetime {
-	// 2018-05-16 03:01:00 +0000 UTC
-	// 2018-05-16 09:29:55.8957322 -0300 -03 m=+0.005003101
-	p := strings.Split(utc, " ")
-	if len(p) < 2 {
-		return nil
-	}
+// String ...
+func (d *Datetime) String() string {
+	tokens := d.extractTokens()
 
-	data := strings.Split(p[0], "-")
-	y := data[0]
-	M := data[1]
-	d := data[2]
-
-	hora := strings.Split(p[1], ".")
-	hora = strings.Split(hora[0], ":")
-	h := hora[0]
-	m := hora[1]
-	s := hora[2]
-
-	time, _ := time.Parse("2006-01-02 15:04:05", str.Format("{0}-{1}-{2} {3}:{4}:{5}", y, M, d, h, m, s))
-	return &Datetime{time: time}
+	return str.Format("{0}-{1}-{2} {3}:{4}:{5}",
+		formatNumber(tokens["y"]),
+		formatNumber(tokens["M"]),
+		formatNumber(tokens["d"]),
+		formatNumber(tokens["h"]),
+		formatNumber(tokens["m"]),
+		formatNumber(tokens["s"]),
+	)
 }
 
 // TotalDias ...
@@ -59,4 +50,36 @@ func (d *Datetime) TotalDias(n *Datetime) int {
 	}
 
 	return t
+}
+
+func formatNumber(n int) string {
+	s := strconv.Itoa(n)
+
+	if n < 10 {
+		return "0" + s
+	}
+
+	return s
+}
+
+func (d *Datetime) extractTokens() map[string]int {
+	tokens := map[string]int{}
+
+	p := strings.Split(d.time.String(), " ")
+	if len(p) < 2 {
+		return tokens
+	}
+
+	data := strings.Split(p[0], "-")
+	tokens["y"], _ = strconv.Atoi(data[0])
+	tokens["M"], _ = strconv.Atoi(data[1])
+	tokens["d"], _ = strconv.Atoi(data[2])
+
+	hora := strings.Split(p[1], ".")
+	hora = strings.Split(hora[0], ":")
+	tokens["h"], _ = strconv.Atoi(hora[0])
+	tokens["m"], _ = strconv.Atoi(hora[1])
+	tokens["s"], _ = strconv.Atoi(hora[2])
+
+	return tokens
 }
