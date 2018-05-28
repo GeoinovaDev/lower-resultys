5e8dc99c059d030b45e7881e29fb429900da4b9d
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-05-28 10:37:56 -0300

    add mutex promise

--- promise/promise.go
+++ promise/promise.go
@@ -1,5 +1,9 @@
 package promise
 
+import (
+	"sync"
+)
+
 // Promise é a estrutura contendo informações execução futura
 type Promise struct {
 	cbOk   []func(interface{})
@@ -12,6 +16,15 @@ type Promise struct {
 
 	obj     interface{}
 	message string
+
+	mutex *sync.Mutex
+}
+
+// New ...
+func New() *Promise {
+	return &Promise{
+		mutex: &sync.Mutex{},
+	}
 }
 
 func (p *Promise) callOk(obj interface{}) {
@@ -58,7 +71,9 @@ func (p *Promise) Reject(message string) *Promise {
 
 // Ok recebe callback de espera que será executado caso houve sucesso na ação
 func (p *Promise) Ok(cb func(interface{})) *Promise {
+	p.mutex.Lock()
 	p.cbOk = append(p.cbOk, cb)
+	p.mutex.Unlock()
 
 	if p.isOk {
 		p.callOk(p.obj)
@@ -69,7 +84,9 @@ func (p *Promise) Ok(cb func(interface{})) *Promise {
 
 // Err recebe callback de espera que será executado caso haja falha
 func (p *Promise) Err(cb func(string)) *Promise {
+	p.mutex.Lock()
 	p.cbErr = append(p.cbErr, cb)
+	p.mutex.Unlock()
 
 	if p.isErr {
 		p.callErr(p.message)
@@ -80,7 +97,9 @@ func (p *Promise) Err(cb func(string)) *Promise {
 
 // Done recebe callback que será executado ao final da ação com sucesso ou falha
 func (p *Promise) Done(cb func()) *Promise {
+	p.mutex.Lock()
 	p.cbDone = append(p.cbDone, cb)
+	p.mutex.Unlock()
 
 	if p.isDone {
 		p.callDone()
