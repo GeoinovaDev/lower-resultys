71218defc439130bc2ad866d3aa26fbdadfb7716
manoelnecojr@gmail <necoengenheiro@gmail.com>
2020-05-14 00:36:48 -0300

    up

--- convert/decode/json.go
+++ convert/decode/json.go
@@ -3,8 +3,6 @@ package decode
 import (
 	"bytes"
 	"encoding/json"
-
-	"git.resultys.com.br/lib/lower/exception"
 )
 
 // JSON decode into object
@@ -13,7 +11,7 @@ func JSON(str string, obj interface{}) (string, bool) {
 	err := json.NewDecoder(b).Decode(&obj)
 
 	if err != nil {
-		exception.Raise(err.Error(), exception.WARNING)
+		// exception.Raise(err.Error(), exception.WARNING)
 		return err.Error(), false
 	}
 
